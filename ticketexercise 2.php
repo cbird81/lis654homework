cf66884b59dda2bd0d17c8b4cba6b642e98ec56b
cibird81 <cibird81@github.com>
2018-01-31 18:01:31 -0500

    first

---
+++ ticketexercise 2.php
@@ -0,0 +1,83 @@
+<?php
+
+$speed = 64;
+$birthday = "0131"
+$current_date = date ("md")
+
+$speed = 85;
+if ($speed < 61) {
+
+  echo "No ticket";
+
+}
+
+elseif ($speed => 61) && ($speed <= 80) {
+   echo "Small ticket";
+} else {
+    echo  "Big ticket";
+}
+
+if ($birthday == current_date)
+   echo  "its your birthday";
+   $speed = $speed -5; 
+
+$counter = 1;
+
+while (($speed > 80 ) && (counter <11));
+   echo "BAD BAD TOO FAST"
+   
+   # multiplication table!
+   
+   for ($count =1,; $ count <=; . $ count *  12)
+   
+   echo    '<table border = 1>';
+   
+           '<tr>';
+           '<td>';
+              $c*1;
+           '</td>' ;
+           '</tr>';
+           
+// or more efficiently 
+
+for ($c = 1; $count <= 12; ++$c)
+{ '<tr>'
+{
+  echo "$count times 12 is ". $count * 12;
+  echo  "<br>";
+}
+ 
+  echo  '</table>';
+?>
+
+$inner _counter = 1;
+$inner_counter <=12, ++$inner_counter) {
+echo '<td style ="color:red;">;
+else 
+echo '<td>';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
